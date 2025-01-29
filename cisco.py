88e57d0b8df338c1713eb145f43ae96e1a736f04
Achacha Hedmon <hedmonachacha@gmail.com>
2025-01-29 11:26:55 +0300

    first commit

---
+++ cisco.py
@@ -0,0 +1,34 @@
+print("Hello , world\n" * 5)
+
+bin(5)
+print(bin(5))
+
+
+print("The itsy bitsy spider climbed up \n the waterspout.")
+print("Down came the rain \n and washed the spider out.")
+print()
+print("Out came the sun \n and dried up all the rain.")
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+print("Programming","Essentials","in" , sep="***", end="...")
+print("Python")
+print("***         ***")
+print("***         ***")
+print("***         ***")
+print("***************")
+print("***************")
+print("***         ***")
+print("***         ***")
+print("***         ***")
+
+print("My\nname\nis\nBond.", end=" ")
+print("James Bond.")
+
+# print(fish", "chips" , sep="&"")
+print("fish", "chips", sep="&")
+
+
