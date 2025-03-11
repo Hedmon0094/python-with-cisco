f99ba8127d4c34d6276763042456bef2d73f49da
Hedmon0094 <hedmonachacha@gmail.com>
2025-03-11 06:22:51 +0300

    started functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+# functions
+def get_user_input():
+    return input("Enter a word: ")
+get_user_input()
+
+def message():    # defining a function
+    print("Hello")    # body of the function
+
+message()    # calling the function
+
